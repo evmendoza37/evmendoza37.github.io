7e6b74c10e8bc578739f86a892ae84c60ca9dfb4
zengruling <52719362+zengruling@users.noreply.github.com>
2019-07-30 09:39:57 -0500

    Created php for contact form

---
+++ action_page.php
@@ -0,0 +1,64 @@
+<?php
+if(isset($_POST['email'])) {
+     
+    // CHANGE THE TWO LINES BELOW
+    $email_to = "czeng888@gmail.com";
+     
+    $email_subject = "FirstGen Message";
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "There are errors in the form you have submitted.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please fix these errors and try again.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['Email']) ||
+        !isset($_POST['Message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+
+    $email_from = $_POST['Email']; // required
+    $comments = $_POST['Message']; // required
+     
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+   
+  if(strlen($comments) < 2) {
+    $error_message .= 'The message you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+    $email_message .= "Email: ".clean_string($email_from)."\n
+    $email_message .= "Message: ".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- place your own success html below -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+}
+die();
+?>
